86cf46ede4fa08e22e482b86b0fa6cf6504a2d30
liuying <liuyingover@163.com>
2018-09-27 22:22:24 +0800

    if and for

---
+++ basic/branch.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"io/ioutil"
+	"fmt"
+)
+
+func eval(a, b int, op string) int {
+	var result int
+	switch op {
+	case "+":
+		result = a + b
+	case "-":
+		result = a - b
+	case "*":
+		result = a * b
+	case "/":
+		result = a / b
+	default:
+		panic("不支持的操作符: " + op)
+	}
+	// switch会自己break 除非使用fallthrough
+	// switch 后面可以没有表达式，可以在case后
+	return result
+}
+
+func grade(score int ) string {
+	g := ""
+	switch {
+	case score<=60:
+		g = "F"
+	case score<80:
+		g="B"
+	case score<=100:
+		g="A"
+	default:
+		panic(fmt.Sprintf("错误的分数: %d", score))
+	}
+	return g
+}
+
+func main() {
+	const filename = "./abc.txt"
+	contents, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%s\n", contents)
+	}
+
+	// 另一种方法
+	/*
+	if 语句中是可以赋值的
+	if 语句中赋值的变量作用域只在这个if语句中
+	*/
+	if contents, err := ioutil.ReadFile(filename); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%s\n", contents)
+	}
+
+	eval(3, 2, "+")
+
+	fmt.Println(grade(79))
+
+	sum := 0
+	/*
+	for的条件里不需要括号
+	for的条件里可以省略初始条件，结束条件，递增表达式
+	*/
+	for i:=1; i<=100; i++{
+		sum +=i
+	}
+	fmt.Println(sum)
+}
---
+++ basic/loop.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"os"
+	"bufio"
+)
+
+// 省略初始条件
+func convertToBin(n int) string {
+	/**
+	整数转为2进制字符串
+	 */
+	result := ""
+	for ; n > 0; n /= 2 {
+		lsb := n % 2
+		result = strconv.Itoa(lsb) + result
+	}
+	return result
+}
+
+// 省略初始条件和递增条件
+func readFile(fileName string)  {
+	file, err := os.Open(fileName)
+	if err!=nil{
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan(){
+		fmt.Println(scanner.Text())
+	}
+}
+
+// 死循环
+func forever()  {
+	for {
+		fmt.Println("forever")
+	}
+}
+
+func main() {
+	fmt.Println(
+		convertToBin(5),
+		convertToBin(13),
+		convertToBin(0),
+	)
+
+	readFile("abc.txt")
+
+	forever()
+}
